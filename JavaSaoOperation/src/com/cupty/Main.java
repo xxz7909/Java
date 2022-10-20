a0334328d490ff373a48ea025a7edc70b5b81f29
xixi <2990698929@qq.com>
2022-10-20 21:32:19 +0800

    add JavaSaoOperation

---
+++ JavaSaoOperation/src/com/cupty/Main.java
@@ -0,0 +1,15 @@
+package com.cupty;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // \u000d  System.out.println("Hello World!");
+        int a=1;
+        // \u000d  \u0061\u002b\u002b\u003b //a++;
+        System.out.println(a);
+
+        int b=3;
+        b>>=32;
+        System.out.println(b);
+    }
+}
